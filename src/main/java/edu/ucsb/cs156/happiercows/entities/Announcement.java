8052a509618fda7aab0fee7d83d67bbbb7f657c9
Alex Xu <alexkxu@169-231-119-189.wireless.ucsb.edu>
2023-11-28 09:39:39 -0800

    created entity and repository files for Announcement

---
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Announcement.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs156.happiercows.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "announcement")
+public class Announcement {
+    
+    // Unique Announcement Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long commonsId;
+    private LocalDateTime start;
+
+    @Column(nullable = true) // end datetime is optional
+    private LocalDateTime end;
+
+    private String announcement;
+}
